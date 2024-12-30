45c59499f1a4781b6b08f66811012f6919c531f1
ryandioneda <er.dioneda@gmail.com>
2024-12-30 09:36:08 -0500

    Added email functionality
    Changes:
    - Added email functionality to email form on contact page
    - Installed @emailjs/browser for functionality

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,28 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+// EmailJS imports
 
 function Contact() {
+
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(import.meta.env.VITE_EMAIL_JS_SERVICE_ID, import.meta.env.VITE_EMAIL_JS_TEMPLATE_ID, form.current, {
+                publicKey: import.meta.env.VITE_EMAIL_JS_PUBLIC_KEY,
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text)
+                },
+            );
+    };
+
     return (
        <div id="contact-wrapper" className="p-2.5 font-inter">
             <div id="main-contact-container" className="p-2 grid grid-cols-1 md:grid-cols-2">
@@ -11,50 +34,53 @@ function Contact() {
 
                 <div id="grid-col2" className="col-span-1 p-1 text-ivory text-[1.15rem] md:text-[1.5rem] md:flex md:justify-start">
                     <div id="form-wrapper" className="md:pr-20">
-                        <div id="form-heading">
-                            <p className="">Feel free to get in touch with any questions.</p>
-                            <p className="mt-2.5">I would love to work together.</p>
-                        </div>
 
-                        <div id="email-form-container" className="w-full mt-10">
-                            <div id="user-info" className="flex flex-row gap-5 w-full">
-                                <div id="name" className="">
-                                    <form action="">
-                                        <div id="input-field" className="">
-                                            <input autoComplete="off" id="name" placeholder="Name" className="text-[1.15rem] md:text-[1.15rem] w-full text-ivory bg-darkgray p-2.5"></input>
-                                        </div>
-                                    </form>
-                                </div>
-
-
-                                <div id="email" className="">
-                                    <form action="">
-                                        <div id="input-field" className="">
-                                            <input autoComplete="off" id="email" placeholder="Email" className="text-[1.15rem] md:text-[1.15rem] w-full text-ivory bg-darkgray p-2.5"></input>
-                                        </div>
-                                    </form>
-                                </div>
-
+                        {/* beginning of form */}
+                        <form ref={form} onSubmit={sendEmail}>
+                            <div id="form-heading">
+                                <p className="">Feel free to get in touch with any questions.</p>
+                                <p className="mt-2.5">I would love to work together.</p>
                             </div>
 
-                            <div id="user-message" className="mt-3.5">
-                                <div id="message">
-                                    <form id="form" action="" >
+                            <div id="email-form-container" className="w-full mt-10">
+                                <div id="user-info" className="flex flex-row gap-5 w-full">
+                                    <div id="name" className="">
+                                        <div id="input-field" className="">
+                                            <input autoComplete="off" id="name" name='user_name' placeholder="Name" className="text-[1.15rem] md:text-[1.15rem] w-full text-ivory bg-darkgray p-2.5"></input>
+                                        </div>
+                                    </div>
+
+
+                                    <div id="email" className="">
+                                        <div id="input-field" className="">
+                                            <input autoComplete="off" id="email" name='user_email' placeholder="Email" className="text-[1.15rem] md:text-[1.15rem] w-full text-ivory bg-darkgray p-2.5"></input>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div id="user-message" className="mt-3.5">
+                                    <div id="message">
                                         <div id="input-field">
-                                            <textarea autoComplete="off" placeholder="Message" id="message" className="text-[1.15rem] md:text-[1.15rem] w-full text-ivory bg-darkgray p-2.5 h-40"></textarea>
+                                            <textarea autoComplete="off" placeholder="Message" id="message" name='message' className="text-[1.15rem] md:text-[1.15rem] w-full text-ivory bg-darkgray p-2.5 h-40"></textarea>
                                         </div>
-                                    </form>
+                                    </div>
+                                </div>
+
+                                <div id="send-button" className="bg-[#0b468c] mt-1.5">
+                                    <button className="w-full p-2.5" type='submit' value="Send">
+                                        Send
+                                    </button>
                                 </div>
                             </div>
-
-                            <div id="send-button" className="bg-[#0b468c] mt-1.5">
-                                <button className="w-full p-2.5">
-                                    Send
-                                </button>
-                            </div>
-
-                        </div>
+                        </form>
                     </div>
+                    {/* end of form */}
+
+
+
+
+
                 </div>
             </div>
        </div>
