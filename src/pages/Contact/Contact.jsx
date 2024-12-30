d87adbe1141ef2bfe071b2f92eec26be0b8f3210
ryandioneda <er.dioneda@gmail.com>
2024-12-30 16:17:49 -0500

    Added validation functionality to email form
    Changes:
    - Added email validation to email contact form with validateEmail()
    - Added email success indicator  by setting button text

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,13 +1,32 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState} from 'react';
 import emailjs from '@emailjs/browser';
-
-// EmailJS imports
 
 function Contact() {
 
     const form = useRef();
+    const emailField = useRef();
+    const [buttonText, setButtonText] = useState("Send")
+    const [isEmailValid, setIsEmailValid] = useState(true);
+
+    const validateEmail = () => {
+        const emailValue = emailField.current.value
+        const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+        return emailPattern.test(emailValue);
+
+    }
+
     const sendEmail = (e) => {
+
         e.preventDefault();
+        setButtonText("Sending...");
+
+        if (!validateEmail()) {
+            setIsEmailValid(false);
+            setButtonText("Send")
+            return
+ 
+        }
+        setIsEmailValid(true);
 
         emailjs
             .sendForm(import.meta.env.VITE_EMAIL_JS_SERVICE_ID, import.meta.env.VITE_EMAIL_JS_TEMPLATE_ID, form.current, {
@@ -15,10 +34,11 @@ function Contact() {
             })
             .then(
                 () => {
-                    console.log('SUCCESS');
+                    setButtonText("Sent!")
+                    setTimeout(() => setButtonText("Send"), 5000); 
                 },
                 (error) => {
-                    console.log('FAILED...', error.text)
+                    setButtonText("Send")
                 },
             );
     };
@@ -36,7 +56,7 @@ function Contact() {
                     <div id="form-wrapper" className="md:pr-20">
 
                         {/* beginning of form */}
-                        <form ref={form} onSubmit={sendEmail}>
+                        <form ref={form} onSubmit={sendEmail} method='POST'>
                             <div id="form-heading">
                                 <p className="">Feel free to get in touch with any questions.</p>
                                 <p className="mt-2.5">I would love to work together.</p>
@@ -46,14 +66,23 @@ function Contact() {
                                 <div id="user-info" className="flex flex-row gap-5 w-full">
                                     <div id="name" className="">
                                         <div id="input-field" className="">
-                                            <input autoComplete="off" id="name" name='user_name' placeholder="Name" className="text-[1.15rem] md:text-[1.15rem] w-full text-ivory bg-darkgray p-2.5"></input>
+                                            <input autoComplete="off" id="name" type='text' name='user_name' placeholder="Name" className="text-[1.15rem] md:text-[1.15rem] w-full text-ivory bg-darkgray p-2.5" required></input>
                                         </div>
                                     </div>
 
 
                                     <div id="email" className="">
-                                        <div id="input-field" className="">
-                                            <input autoComplete="off" id="email" name='user_email' placeholder="Email" className="text-[1.15rem] md:text-[1.15rem] w-full text-ivory bg-darkgray p-2.5"></input>
+                                        <div id="email-field" className="">
+                                            <input
+                                                ref={emailField} 
+                                                autoComplete="off" 
+                                                id="email" 
+                                                type='email' 
+                                                name='user_email' 
+                                                placeholder="Email" 
+                                                className={`text-[1.15rem] md:text-[1.15rem] w-full text-ivory bg-darkgray p-2.5 ${!isEmailValid ? 'border-2 border-red-500' : ''}`}
+                                                required>
+                                            </input>
                                         </div>
                                     </div>
 
@@ -61,27 +90,23 @@ function Contact() {
 
                                 <div id="user-message" className="mt-3.5">
                                     <div id="message">
-                                        <div id="input-field">
-                                            <textarea autoComplete="off" placeholder="Message" id="message" name='message' className="text-[1.15rem] md:text-[1.15rem] w-full text-ivory bg-darkgray p-2.5 h-40"></textarea>
+                                        <div id="message-field">
+                                            <textarea autoComplete="off" placeholder="Message" id="message" name='message' className="text-[1.15rem] md:text-[1.15rem] w-full text-ivory bg-darkgray p-2.5 h-40" required></textarea>
                                         </div>
                                     </div>
                                 </div>
 
                                 <div id="send-button" className="bg-[#0b468c] mt-1.5">
                                     <button className="w-full p-2.5" type='submit' value="Send">
-                                        Send
+                                        {buttonText}
                                     </button>
                                 </div>
                             </div>
                         </form>
                     </div>
                     {/* end of form */}
-
-
-
-
-
                 </div>
+
             </div>
        </div>
     )
