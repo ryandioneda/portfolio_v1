a9eb4af07aaaa0a086a5c871b6103f178914598a
ryandioneda <er.dioneda@gmail.com>
2025-01-01 23:29:01 -0500

    Added lenis scroll for smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import "@fontsource/inter";
+import Lenis from 'lenis';
 
 // common app components imports
 import Nav from './components/Nav/Nav';
@@ -9,8 +12,23 @@ import Footer from './components/Footer/Footer';
 import Index from './pages/Index/Index';
 import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
+import Work from './pages/Work/Work';
+import { useEffect } from 'react';
+import 'lenis/dist/lenis.css';
 
 function App() {
+  
+  useEffect( () => {
+    const lenis = new Lenis()
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
+
   return (
     <div className='flex flex-col min-h-screen'>
       <BrowserRouter>
@@ -21,6 +39,7 @@ function App() {
             <Route path='/' element={<Index />} />
             <Route path='/about' element={<About />} />
             <Route path='/contact' element={<Contact />} />
+            <Route path='/work' element={<Work />} />
           </Routes>
           
         </div>
